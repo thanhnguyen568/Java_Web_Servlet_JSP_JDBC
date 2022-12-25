945844956a28925c90759df5b973d6f46d1e30d9
thanhnguyen568 <thanhnguyen230@gail.com>
2022-12-25 23:15:51 +0700

    up date case study task 1,2,3,4

--- Case_Study_DB/Case_Study_DB.sql
+++ Case_Study_DB/Case_Study_DB.sql
@@ -207,7 +207,7 @@ values
 ('Thuê xe máy',	10000,'chiếc','hỏng 1 xe'),
 ('Thuê xe đạp',20000,'chiếc','tốt'),
 ('Buffet buổi sáng',15000,'suất','đầy đủ đồ ăn tráng miệng'),
-(',Buffet buổi trưa',90000,'suất', 'đầy đủ đồ ăn tráng miệng'),
+('Buffet buổi trưa',90000,'suất', 'đầy đủ đồ ăn tráng miệng'),
 ('Buffet buổi tối',16000,'suất', 'đầy đủ đồ ăn tráng miệng');
 
 /*
@@ -269,8 +269,79 @@ values
 (2,1,2),
 (2,12,2);
 
-/* Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là
+/* Task2 - Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là
 một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.*/
 
-select * from nhan_vien
-where ho_ten like 'H______________';
+select *
+from nhan_vien
+where ho_ten like 'H%' and char_length(ho_ten) <=15
+union
+select *
+from nhan_vien
+where ho_ten like 'T%' and char_length(ho_ten) <=15
+union
+select *
+from nhan_vien
+where ho_ten like 'K%' and char_length(ho_ten) <=15;
+
+/* Task3 - Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi 
+và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”. */
+
+select ma_khach_hang,ho_ten,gioi_tinh,timestampdiff(year,ngay_sinh,now()) as age,so_dien_thoai,email,dia_chi
+from khach_hang
+where dia_chi like '%Đà Nẵng'
+group by ma_khach_hang
+having age between 18 and 50
+union
+select ma_khach_hang,ho_ten,gioi_tinh,timestampdiff(year,ngay_sinh,now()) as age,so_dien_thoai,email,dia_chi
+from khach_hang
+where dia_chi like '%Quảng Trị'
+group by ma_khach_hang
+having age between 18 and 50
+order by ma_khach_hang;
+
+/*Task4 - Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. 
+Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. 
+Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
+*/
+
+select k.ma_khach_hang,k.ho_ten,count(ma_hop_dong) as sl_hop_dong
+from khach_hang k
+inner join hop_dong h
+on k.ma_khach_hang = h.ma_khach_hang
+inner join loai_khach l
+on k.ma_loai_khach = l.ma_loai_khach
+where l.ten_loai_khach = 'diamond'
+group by k.ma_khach_hang
+order by sl_hop_dong asc;
+
+/*Task5 - Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien 
+(Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) 
+cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+*/
+
+select k.ma_khach_hang, k.ho_ten, l.ten_loai_khach, h.ma_hop_dong, d.ten_dich_vu, h.ngay_lam_hop_dong, h.ngay_ket_thuc, chi_phi_thue+(so_luong*gia) as tong_tien
+from loai_khach l
+inner join khach_hang k
+using (ma_loai_khach)
+inner join hop_dong h
+using (ma_khach_hang)
+inner join dich_vu d
+using (ma_dich_vu)
+inner join hop_dong_chi_tiet hd
+using (ma_hop_dong)
+inner join dich_vu_di_kem dv
+using (ma_dich_vu_di_kem)
+order by k.ma_khach_hang;
+
+/*Task6 - Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu 
+của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+*/
+
+select ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu
+from dich_vu
+inner join loai_dich_vu
+using (ma_loai_dich_vu)
+inner join hop_dong
+using (ma_dich_vu)
+
