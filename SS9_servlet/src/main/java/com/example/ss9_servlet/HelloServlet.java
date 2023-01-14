1baddbce80a636d5373667b7f7e05f220d5856c5
thanhnguyen568 <thanhnguyen230@gmail.com>
2023-01-14 12:39:07 +0700

    update ss9

---
+++ SS9_servlet/src/main/java/com/example/ss9_servlet/HelloServlet.java
@@ -0,0 +1,33 @@
+package com.example.ss9_servlet;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    public void destroy() {
+    }
+}
